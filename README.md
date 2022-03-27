7cc05539e99e4c2e1170170de9f9c6bc107031b9
Sonjieun <sonjieun2661@gmail.com>
2022-03-27 20:23:40 +0900

    +error msg, room name

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # webcam_chatting
 
 Zoom clone using NodeJS, WebRTC and Websockets
+
+1. room 이름을 통해 채팅방 생성 및 참여
+2. 카메라, 오디오 요청
+3. 자신의 화면과 음소거버튼, 화면전환 버튼, 카메라 목록을 보여줌
+4. 상대방이 입장하면 자신의 화면 옆에 상대방의 화면 출력
+5. 텍스트 채팅 가능
+
+#nomadcoder
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -91,6 +91,8 @@ async function handleCameraChange() {
       .getSenders()
       .find((sender) => sender.track.kind === "video");
     videoSender.replaceTrack(videoTrack);
+  } else {
+    console.log("nobody");
   }
 }
 
@@ -117,6 +119,11 @@ async function handleWelcomeSubmit(event) {
   await initCall();
   socket.emit("join_room", input.value);
   roomName = input.value;
+
+  const head = document.querySelector("body header");
+  const header = document.createElement("h2");
+  header.innerText = `Room: ${roomName}`;
+  head.appendChild(header);
   input.value = "";
 }
 
@@ -200,8 +207,14 @@ function handleSendMsg(event) {
   event.preventDefault();
   const input = chatForm.querySelector("input");
   message = input.value;
-  myDataChannel.send(input.value);
-  showMsg(`You: ${input.value}`);
+  try {
+    myDataChannel.send(input.value);
+    showMsg(`You: ${input.value}`);
+  } catch (e) {
+    console.log(e);
+    showMsg("Nobody is here!!");
+  }
+
   input.value = "";
 }
 
@@ -213,172 +226,3 @@ function showMsg(event) {
 }
 const chatForm = chat.querySelector("form");
 chatForm.addEventListener("submit", handleSendMsg);
-
-//-------------------------chatting : using socket.io-------------------------------------------------
-//------home.pug-----
-// div#welcome
-//                 form
-//                     input(placeholder="room name", required, type="text")
-//                     button Enter Room
-//                 h4 Open Rooms
-//                 ul
-//             div#room
-//                 h3
-//                 ul
-//                 form#nickname
-//                     input(placeholder="nickname", required, type="text")
-//                     button Ok
-//                 form#msg
-//                     input(placeholder="message", required, type="text")
-//                     button Send
-
-//------app.js----------------
-// const socket = io(); // io: 자동적으로 backend socket.io와 연결
-
-// //room
-// const welcome = document.getElementById("welcome");
-// const form = welcome.querySelector("form");
-
-// //msg
-// const room = document.getElementById("room");
-// const nick = document.getElementById("nickname");
-// const msgBox = document.getElementById("msg");
-
-// let roomName;
-
-// room.hidden = true; //방 입장 전
-
-// //화면에 msg출력
-// function addMsg(message) {
-//   const ul = room.querySelector("ul");
-//   const li = document.createElement("li");
-//   li.innerText = message;
-//   ul.appendChild(li);
-// }
-
-// //send msg
-// function handleMsgSubmit(event) {
-//   event.preventDefault();
-//   const input = room.querySelector("#msg input");
-//   const msg = input.value; //input value를 비우기전의 값을 넘겨주기 위해
-//   socket.emit("message", roomName, input.value, () => {
-//     addMsg(`You: ${msg}`);
-//   });
-//   input.value = "";
-// }
-
-// //nickname
-// function handleNickNameSubmit(event) {
-//   event.preventDefault();
-//   msgBox.hidden = false;
-
-//   const input = room.querySelector("#nickname input");
-//   socket.emit("nickname", input.value);
-//   socket.emit("enter_room", roomName, showRoom);
-// }
-
-// //room
-// function showRoom() {
-//   const h3 = room.querySelector("h3");
-//   h3.innerText = `Room: ${roomName}`;
-//   const msgForm = room.querySelector("#msg");
-//   msgForm.addEventListener("submit", handleMsgSubmit);
-// }
-
-// //send roomname
-// function handleRoomSubmit(event) {
-//   event.preventDefault();
-//   const input = form.querySelector("input");
-
-//   welcome.hidden = true;
-//   room.hidden = false;
-//   msgBox.hidden = true;
-
-//   const nickNameForm = room.querySelector("#nickname");
-//   nickNameForm.addEventListener("submit", handleNickNameSubmit);
-//   //socket.io 는 특정 event를 emit 할 수 있음, object 전달 가능, 원하는 정보 모두 전달 가능
-//   //socket.emit(name,payload,etc,,,,,fn(for server))
-
-//   roomName = input.value;
-//   input.value = "";
-// }
-
-// function changeRoomTitle(newCount) {
-//   const h3 = room.querySelector("h3");
-//   h3.innerText = `Room: ${roomName} (${newCount})`;
-// }
-
-// form.addEventListener("submit", handleRoomSubmit);
-
-// socket.on("welcome", (user, newCount) => {
-//   changeRoomTitle(newCount);
-//   addMsg(`${user} joined.`);
-// });
-
-// socket.on("bye", (user, newCount) => {
-//   changeRoomTitle(newCount);
-//   addMsg(`${user} left.`);
-// });
-
-// socket.on("message", addMsg);
-
-// socket.on("room_change", (rooms) => {
-//   const roomList = welcome.querySelector("ul");
-//   roomList.innerHTML = "";
-//   if (rooms.length === 0) {
-//     //방이 삭제됐을때 업데이트
-//     return;
-//   }
-//   rooms.forEach((room) => {
-//     const li = document.createElement("li");
-//     li.innerText = room;
-//     roomList.append(li);
-//   });
-// });
-//------------------using wss-----------------------------------------------------------------
-// const socket = new WebSocket(`ws://${window.location.host}`); //socket=서버로의 연결
-// const nickname = document.querySelector("#nickname");
-// const messageList = document.querySelector("ul");
-// const messageForm = document.querySelector("#msg");
-
-// socket.addEventListener("open", () => {
-//   console.log("Connected to Server ✔");
-// });
-
-// //disconnect to server
-// socket.addEventListener("close", () => {
-//   console.log("Disconnected to Server ❌");
-// });
-
-// //receive msg
-// socket.addEventListener("message", (message) => {
-//   const li = document.createElement("li");
-//   li.innerText = message.data;
-//   messageList.append(li);
-// });
-
-// //object를 string형태로 backend에 전달 : 모든 서버가 읽을 수 있는 형태로 전달하기 위해
-// function makeMessage(type, payload) {
-//   const msg = { type, payload };
-//   return JSON.stringify(msg);
-// }
-
-// //nick submit event
-// function handleNickname(event) {
-//   event.preventDefault();
-//   const input = nickname.querySelector("input");
-//   //send JSON Object
-//   socket.send(makeMessage("nickname", input.value));
-//   input.value = "";
-// }
-
-// //msg submit event
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const input = messageForm.querySelector("input");
-//   socket.send(makeMessage("new_message", input.value)); //browser => server
-//   input.value = "";
-// }
-
-// nickname.addEventListener("submit", handleNickname);
-// messageForm.addEventListener("submit", handleSubmit);
